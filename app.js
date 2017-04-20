317ce02f1979f0fe3b62e6af3e1568d01b685033
cyberplanner <sal@salweb.co.uk>
2017-04-20 16:43:36 +0100

    Test logging dirname and filename

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Node is working!!');
+// setTimeout(function() {
+//   console.log("3 secondes has passed");
+// }, 3000);
+
+// var time = 0;
+//
+// var timer = setInterval(function() {
+//               time += 2;
+//               console.log(time + " seconds has passed!");
+//               if(time > 5) {
+//                 clearInterval(timer)
+//               }
+//             }, 2000);
+console.log(__dirname);
+console.log(__filename);
