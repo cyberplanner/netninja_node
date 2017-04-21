6d6077a2ae7a5be5e328fabac24ff68cbf97e055
cyberplanner <sal@salweb.co.uk>
2017-04-21 10:07:35 +0100

    Ajax requests.

---
+++ src/async.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  // var http = new XMLHttpRequest();
+  // http.onreadystatechange = function() {
+  //   if(http.readyState == 4 && http.status == 200) {
+  //     // console.log(JSON.parse(http.response));
+  //   }
+  // };
+  // http.open("GET", "data/tweets.json", true);
+  // http.send();
+  //
+  // // jQuery method
+  //
+  // $.get("data/tweets.json", function(data) {
+  //   console.log(data);
+  // });
+
+  // $.ajax({
+  //   url: "http://aston-user-service.eu-gb.mybluemix.net/user/DanCotton",
+  //   type: 'GET',
+  //   // Fetch the stored token from localStorage and set in the header
+  //   headers: {"Authorization": localStorage.getItem('token')}
+  // });
+};
